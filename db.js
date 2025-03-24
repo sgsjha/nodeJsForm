df2d0c2b17358d14dfce37e39a3ab9bca02cce4c
sgsjha <sarthakjhaa11@gmail.com>
2025-03-24 09:02:47 +0000

    fix

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const { MongoClient } = require('mongodb');
 
 // Replace with your actual MongoDB connection string
-const uri = "mongodb+srv://sarthakjhaa11:57I7mh0WmPLa1D0J@cluster0.ql78g.mongodb.net/testDB?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 
 async function withDb(operation) {
   const client = new MongoClient(uri);
